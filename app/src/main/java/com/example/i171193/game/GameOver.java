05255e0c6de26f4be341cdcc3c502b521d8a7596
i171193 <quentin.pineau.etu@univ-lemans.fr>
2018-11-13 16:12:38 +0100

    best score

--- app/src/main/java/com/example/i171193/game/GameOver.java
+++ app/src/main/java/com/example/i171193/game/GameOver.java
@@ -1,6 +1,8 @@
 package com.example.i171193.game;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,13 +14,19 @@ public class GameOver extends AppCompatActivity {
     private Button rejouer;
     private Button menu;
     private TextView scoreJoueur;
+    private TextView meilleurScore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_over);
+        this.getMeilleurScore();
         scoreJoueur = (TextView) findViewById(R.id.affScore);
         scoreJoueur.setText("Score : " + MainActivity.score);
+        meilleurScore = (TextView) findViewById(R.id.meilleurScore);
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        int bestScore = preferences.getInt("MeilleurScore",0);
+        meilleurScore.setText("Meilleur score : " + bestScore);
         rejouer = (Button) findViewById(R.id.rejouer);
         rejouer.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,5 +51,14 @@ public class GameOver extends AppCompatActivity {
     public void menu(){
         Intent intent = new Intent(this, Menu.class);
         startActivity(intent);
+    }
+
+    public void getMeilleurScore(){
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        int bestScore = preferences.getInt("MeilleurScore",0);
+        if(bestScore < MainActivity.score){
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putInt("MeilleurScore",MainActivity.score);
+        }
     }
 }
